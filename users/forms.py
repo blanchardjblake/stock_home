56c1305bbedfa5d40e915e944dadb06b3dd61393
Seshagiri Prabhu <seshagiriprabhu@gmail.com>
2021-02-25 11:40:45 -0600

    Adding custom user form

---
+++ users/forms.py
@@ -0,0 +1,34 @@
+"""Forms for accounts app."""
+from django import forms
+from bootstrap_datepicker_plus import DatePickerInput
+from .models import CustomUser
+from django.contrib.auth.forms import (UserCreationForm, UserChangeForm)
+
+
+class CustomUserCreationForm(UserCreationForm):
+    """Registration form."""
+
+    class Meta:
+        """Meta class."""
+
+        model = CustomUser
+        fields = ("first_name", "last_name", "email", "date_of_birth",
+                  "password1", "password2")
+        widgets = {
+            'first_name': forms.TextInput(attrs={"class": "form-control"}),
+            'last_name': forms.TextInput(attrs={"class": "form-control"}),
+            'email': forms.TextInput(attrs={"class": "form-control"}),
+            'date_of_birth': DatePickerInput(),
+            'password1': forms.TextInput(attrs={"class": "form-control"}),
+            'password2': forms.TextInput(attrs={"class": "form-control"}),
+        }
+
+
+class CustomUserChangeForm(UserChangeForm):
+    """User profile change view."""
+
+    class Meta:
+        """Meta class."""
+
+        model = CustomUser
+        fields = ('email',)
