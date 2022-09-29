1dbdb0b3a251ceec3eaa5d96181b7dfcedb740f0
Seshagiri Prabhu Narasimha <seshagiri.narasimha1@louisiana.edu>
2022-09-28 21:53:21 -0500

    Adding dockerfile to build and run the projectg

---
+++ gunicorn.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+export NAME="Django Project Template"   # Django application name
+export DIR=/usr/src/django_project/     # Directory where project is located in the docker
+export USER=django_user                 # User to run this script as
+export WORKERS=4                        # Number of workers that Gunicorn should spawn
+export HOST=0.0.0.0
+export PORT=8080
+export DJANGO_WSGI_MODULE=django_project.wsgi     # Which WSGI file should use
+export LOG_LEVEL=info
+
+gunicorn ${DJANGO_WSGI_MODULE} \
+    --preload \
+    --reload \
+    --chdir=$DIR \
+    --bind=$HOST:$PORT \
+    --workers=$WORKERS \
+    --log-level=$LOG_LEVEL
