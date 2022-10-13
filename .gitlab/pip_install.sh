ff36408cab90f8ad847fb25c0dcbc24ab7360b9c
Seshagiri Prabhu Narasimha <seshagiri.narasimha1@louisiana.edu>
2022-10-12 21:30:09 -0500

    Moving to the cache of pip and venv to the global setting

--- .gitlab/pip_install.sh
+++ .gitlab/pip_install.sh
@@ -3,3 +3,5 @@ virtualenv venv
 source venv/bin/activate
 pip3 install --upgrade -q pip
 pip3 install --no-cache-dir -qr requirements.txt
+python3 manage.py migrate >/dev/null
+python3 manage.py collectstatic --no-input >/dev/null
