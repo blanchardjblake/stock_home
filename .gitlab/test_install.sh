fd0a3cd5959c49a1eaa1d4f407b2030e8b0cd2a6
Seshagiri Prabhu Narasimha <seshagiri.narasimha1@louisiana.edu>
2022-10-12 19:15:44 -0500

    Making pip install quiet and redirecting the output of collectstatic and migrate to /dev/null for the pipeline script

--- .gitlab/test_install.sh
+++ .gitlab/test_install.sh
@@ -1,3 +1,4 @@
-pip3 install --no-cache-dir -r requirements.txt
-python3 manage.py migrate
-python3 manage.py collectstatic --no-input
+pip install --upgrade -q pip
+pip3 install --no-cache-dir -qr requirements.txt
+python3 manage.py migrate >/dev/null
+python3 manage.py collectstatic --no-input >/dev/null
