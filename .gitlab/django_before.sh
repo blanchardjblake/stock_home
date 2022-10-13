91b32f59a59e941e6635671dd9fcdb969c9a838e
Seshagiri Prabhu Narasimha <seshagiri.narasimha1@louisiana.edu>
2022-10-12 21:02:29 -0500

    Testing the venv again

--- .gitlab/django_before.sh
+++ .gitlab/django_before.sh
@@ -1,2 +1,3 @@
+source venv/bin/activate
 python3 manage.py migrate >/dev/null
 python3 manage.py collectstatic --no-input >/dev/null
--- .gitlab/pip_install.sh
+++ .gitlab/pip_install.sh
@@ -1,2 +1,5 @@
-pip install --root-user-action=ignore --upgrade -q pip
-pip3 install --root-user-action=ignore --no-cache-dir -qr requirements.txt
+pip3 install --root-user-action=ignore -r virtualenv
+virtualenv venv
+source venv/bin/activate
+pip3 install --upgrade -q pip
+pip3 install --no-cache-dir -qr requirements.txt
