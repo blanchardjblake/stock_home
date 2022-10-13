949694670a72c361b23178900dd0ed67080b0cfe
Seshagiri Prabhu Narasimha <seshagiri.narasimha1@louisiana.edu>
2022-10-12 19:09:09 -0500

    Improving the doc strings of the tests

--- users/tests.py
+++ users/tests.py
@@ -1,4 +1,4 @@
-"""Tests for users app."""
+"""Tests for `users` app."""
 from http import HTTPStatus
 
 import pytest
@@ -11,33 +11,64 @@ from users.models import CustomUser
 
 
 class UserManagerTestCase(TestCase):
-    """Test `CustomUserManager` class."""
+    """Test for `CustomUserManager` class."""
 
     def test_create_normal(self):
-        """Test `create_user` function."""
+        """Test `create_user()`.
+
+        Tests if `create_user()` creates a `CustomUser` model object.
+        Tests if the `__str__()` of `CustomUser` returns the email address.
+
+        """
         user = CustomUser.objects.create_user(email="jdoe@gmail.com", password="password123")
         self.assertTrue(isinstance(user, CustomUser))
         self.assertEqual(str(user), "jdoe@gmail.com")
 
+    def test_create_user_email_none_raises_value_error(self):
+        """Test `create_user()`.
+
+        Tests if `create_user()` raises `ValueError` when the `email` is `None`.
+
+        """
         with pytest.raises(ValueError) as exc_info:
-            user = CustomUser.objects.create_user(email=None, password=None)
+            CustomUser.objects.create_user(email=None, password=None)
         self.assertEqual(exc_info.type, ValueError)
 
     def test_create_superuser(self):
-        """Test `create_superuser` function."""
+        """Test `create_superuser()`.
+
+        Tests if `create_superuser()` creates a `CustomUser` model object.
+        Tests if the `__str__()` of `CustomUser` returns the email address.
+
+        """
         user = CustomUser.objects.create_superuser(email="jdoe@gmail.com", password="password123")
         self.assertTrue(isinstance(user, CustomUser))
+        self.assertEqual(str(user), "jdoe@gmail.com")
 
+    def test_create_superuser_raises_value_error_is_staff_false(self):
+        """Test for `create_superuser().
+
+        Tests if `create_superuser()` raises `ValueError` when `is_staff` argument
+        to the function is set `False`.
+
+        """
         with pytest.raises(ValueError) as exc_info:
-            user = CustomUser.objects.create_superuser(
+            CustomUser.objects.create_superuser(
                 email="jdoe1@gmail.com",
                 password="password123",
                 is_staff=False,
             )
         self.assertEqual(exc_info.type, ValueError)
 
+    def test_create_superuser_raises_value_error_is_superuser_false(self):
+        """Test for `create_superuser().
+
+        Tests if `create_superuser()` raises `ValueError` when `is_superuser` argument
+        to the function is set `False`.
+
+        """
         with pytest.raises(ValueError) as exc_info:
-            user = CustomUser.objects.create_superuser(
+            CustomUser.objects.create_superuser(
                 email="jdoe1@gmail.com",
                 password="password123",
                 is_superuser=False,
@@ -48,14 +79,14 @@ class UserManagerTestCase(TestCase):
 class CustomUserTestCase(TestCase):
     """Tests for `CustomeUser` model."""
 
-    def test_create_user(self):
-        """Test to check if `CustomUser` create method is working."""
+    def test_create(self):
+        """Tests if `CustomUser`'s `create()` method is working using a query."""
         user = CustomUser.objects.create(email="jdoe@gmail.com", password="password123")
         query = CustomUser.objects.get(email="jdoe@gmail.com")
         self.assertEqual(user, query)
 
     def test_create_user_rasies_not_unique_integrity_error_with_same_email(self):
-        """Test to check if `CustomUser` create method raises Integrity error."""
+        """Tests if `CustomUser`'s `create()` method raises an `IntegrityError`."""
         CustomUser.objects.create(email="jdoe@gmail.com", password="password123")
 
         with pytest.raises(IntegrityError) as exc_info:
@@ -65,10 +96,6 @@ class CustomUserTestCase(TestCase):
 
 class SignUpViewTestCase(TestCase):
     """Tests for `SignUpView` view."""
-
-    def setUp(self):
-        """Set up test data for the class."""
-        self.signup_url = reverse("users:signup")
 
     @parameterized.expand(
         [
@@ -95,25 +122,27 @@ class SignUpViewTestCase(TestCase):
         ]
     )
     def test_signup_view(self, params, status_code):
-        """Test sign up view for strong and weak passwords.
+        """Test `SignUpView` with strong and weak passwords.
 
-        Registration is successful with strong passwords.
-        It fails while registering with weak passwords.
+        Tests if registration is successful for strong passwords.
+        Tests if registration fails for weak passwords.
 
         """
-        response = self.client.post(self.signup_url, data=params)
+        signup_url = reverse("users:signup")
+        response = self.client.post(signup_url, data=params)
         self.assertEqual(response.status_code, status_code)
 
 
 class LoginTestCase(TestCase):
-    """Tests for `Login` view."""
+    """Tests for Django's built-in `Login` view."""
 
     def setUp(self):
         """Set up test data for the class."""
-        CustomUser.objects.create_user(email="jdoe@gmail.com", password="p@$$W0RDL@rG3")
         self.login_url = reverse("login")
         self.logout_url = reverse("logout")
+
         self.login_params = {"username": "jdoe@gmail.com", "password": "p@$$W0RDL@rG3"}
+        CustomUser.objects.create_user(email="jdoe@gmail.com", password="p@$$W0RDL@rG3")
 
     def test_signinview(self):
         """Test `SignUpView`, it should redirect."""
@@ -123,12 +152,12 @@ class LoginTestCase(TestCase):
         response = self.client.get(reverse("home"))
         self.assertEqual(str(response.context.get("user")), "jdoe@gmail.com")
 
-    def test_logged_in_uses_correct_template(self):
-        """Test if logged in user sees the about page and user is set correctly."""
+    def test_logged_in_user_sees_correct_template(self):
+        """Test if logged in user sees the password change page."""
         response = self.client.login(email="jdoe@gmail.com", password="p@$$W0RDL@rG3")
-        response = self.client.get(reverse("about"))
+        response = self.client.get(reverse("password_change"))
         self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertTemplateUsed(response, "about.html")
+        self.assertTemplateUsed(response, "registration/password_change.html")
 
     def test_signinview_invalid_user_password(self):
         """Test login by providing invalid user credentials."""
@@ -145,7 +174,11 @@ class LoginTestCase(TestCase):
         self.assertEqual(str(response.context.get("user")), "AnonymousUser")
 
     def test_login_and_logout(self):
-        """Test logout by login in first and then logout."""
+        """Test logout by login in first and then logout.
+
+        Tests when a user logs out, the `user` object in the `Response` is "AnyonymousUser".
+
+        """
         response = self.client.post(self.login_url, data=self.login_params)
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
 
