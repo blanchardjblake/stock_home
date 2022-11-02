654e08d7dcc6a4891a9e6bfd642a941074f2a8a8
Blake <blanchardjblake@gmail.com>
2022-11-02 16:48:07 -0500

    Another attempt to get the linter to accept.

--- stock_home/apps.py
+++ stock_home/apps.py
@@ -1,6 +1,7 @@
 """Project apps."""
 from django.apps import AppConfig
 
-class StockHomeConfig(AppConfig): """Main app."""
+class StockHomeConfig(AppConfig):
+    """Main app."""
     default_auto_field = "django.db.models.BigAutoField"
     name = "stock_home"
