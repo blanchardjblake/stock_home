a3c192b787202df1c063bbc81aff24dd5e54543c
theokoles <gabrieltrahan777@hotmail.com>
2022-11-06 12:38:37 -0600

    Registers models in admin file

--- stock_home/admin.py
+++ stock_home/admin.py
@@ -1,2 +1,8 @@
 """Admin."""
 from django.contrib import admin
+
+from stock_home.models import Company, Position, Transaction
+
+admin.site.register(Company)
+admin.site.register(Position)
+admin.site.register(Transaction)
