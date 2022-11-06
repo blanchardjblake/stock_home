797ed8f562ec627ccbd0678d8b75ce19c412f5a7
theokoles <gabrieltrahan777@hotmail.com>
2022-11-06 12:38:21 -0600

    Creates Company, Position, and Transaction models

--- stock_home/models.py
+++ stock_home/models.py
@@ -1,4 +1,118 @@
-"""Models (Entities)."""
+"""Stock Home Models (Entities)."""
 from django.db import models
+from users.managers import CustomUserManager
+from users.models import CustomUser
 
-# Create your models here.
+
+class Company(models.Model):
+    """Company model.
+
+    Arguments:
+    ---------
+    Model : class
+        Django's `Model` class.
+
+    Returns:
+    -------
+    object:
+        Model.
+
+    """
+
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField("Company's name", max_length=30, unique=True)
+    value = models.FloatField()
+    share_price = models.FloatField()
+    curr_day_open = models.FloatField()
+    prev_day_open = models.FloatField()
+    curr_day_high = models.FloatField()
+    corr_day_low = models.FloatField()
+    year_high = models.FloatField()
+    year_low = models.FloatField()
+    div_yield = models.DecimalField(decimal_places=4, max_digits=4)
+    volume = models.PositiveIntegerField()
+    avg_volume = models.PositiveIntegerField()
+
+    REQUIRED_FIELDS = [name, share_price]
+
+    def __str__(self) -> str:
+        """Returns string representation of the object.
+
+        Returns:
+        -------
+        str
+            The unique identifer of the model, `name`.
+
+        """
+        return self.name
+
+
+class Position(models.Model):
+    """Position model.
+
+    Arguments:
+    ---------
+    Model : class
+        Django's `Model` class.
+
+    Returns:
+    -------
+    object:
+        Model.
+
+    """
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    quantity = models.FloatField()
+    avg_cost = models.FloatField()
+    p_l = models.FloatField()
+
+    REQUIRED_FIELDS = [user, company, quantity, avg_cost]
+
+    def __str__(self) -> str:
+        """Returns string representation of the object.
+
+        Returns:
+        -------
+        str
+            The quantity and company of the position.
+
+        """
+        return self.quantity + " " + self.company
+
+
+class Transaction(models.Model):
+    """Transaction model.
+
+    Arguments:
+    ---------
+    Model : class
+        Django's `Model` class.
+
+    Returns:
+    -------
+    object:
+        Model.
+
+    """
+
+    user = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
+    company = models.ForeignKey(Company, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField()
+    type = [("Buy", "Buy"), ("Sell", "Sell")]
+    price = models.FloatField()
+    date = models.DateTimeField
+
+    REQUIRED_FIELDS = [user, company, quantity, type, price, date]
+
+    def __str__(self) -> str:
+        """Returns string representation of the object.
+
+        Returns:
+        -------
+        str
+            The quantity and company of the position.
+
+        """
+        return self.quantity + " " + self.company
