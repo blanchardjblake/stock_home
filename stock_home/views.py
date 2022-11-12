230d567f04ac5630e20baab1461b6defcb955afb
theokoles <gabrieltrahan777@hotmail.com>
2022-11-12 11:09:33 -0600

    Django Lint errors

--- stock_home/views.py
+++ stock_home/views.py
@@ -7,14 +7,14 @@ from stock_home.models import Company, Position, Transaction
 
 # -------------------------------------------------- USERS
 class UserListView(generic.ListView):
-    """User List class"""
+    """User List class."""
 
     model = CustomUser
     template_name = "users/custom_user_list.html"
 
 
 class UserDetailView(generic.DetailView):
-    """User Detail class"""
+    """User Detail class."""
 
     model = CustomUser
     template_name = "users/custom_user_detail.html"
@@ -22,14 +22,14 @@ class UserDetailView(generic.DetailView):
 
 # -------------------------------------------------- COMPANIES
 class CompanyListView(generic.ListView):
-    """Company List class"""
+    """Company List class."""
 
     model = Company
     template_name = "stock_home/company_list.html"
 
 
 class CompanyDetailView(generic.DetailView):
-    """Company Detail class"""
+    """Company Detail class."""
 
     model = Company
     template_name = "stock_home/company_detail.html"
@@ -37,14 +37,14 @@ class CompanyDetailView(generic.DetailView):
 
 # -------------------------------------------------- POSITIONS
 class PositionListView(generic.ListView):
-    """Position List class"""
+    """Position List class."""
 
     model = Position
     tempate_name = "stock_home/position_list.html"
 
 
 class PositionDetailView(generic.DetailView):
-    """Position Detail class"""
+    """Position Detail class."""
 
     model = Position
     template_name = "stock_home/position_detail.html"
@@ -52,14 +52,14 @@ class PositionDetailView(generic.DetailView):
 
 # -------------------------------------------------- TRANSACTIONS
 class TransactionListView(generic.ListView):
-    """Transaction List class"""
+    """Transaction List class."""
 
     model = Transaction
     template_name = "stock_home/transaction_list.html"
 
 
 class TransactionDetailView(generic.DetailView):
-    """Transaction Detail class"""
+    """Transaction Detail class."""
 
     model = Transaction
     template_name = "stock_home/transaction_detail.html"
