647674bf9d1297ad70fede2eb027a4571377f15a
Blake <blanchardjblake@gmail.com>
2022-11-18 20:53:41 -0600

    Linter.

--- stock_home/forms.py
+++ stock_home/forms.py
@@ -12,7 +12,7 @@ class CompanyCreateForm(ModelForm):
         model = Company
         fields = (
             "name", "symbol", "value", "share_price", "curr_day_open", "prev_day_open",
-            "curr_day_high","curr_day_low", "year_high", "year_low", "div_yield",
+            "curr_day_high", "curr_day_low", "year_high", "year_low", "div_yield",
             "volume", "avg_volume"
         )
 
