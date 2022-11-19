aa5f68c4ecc3c6bf58c4c7eef18739ee786db8ba
theokoles <gabrieltrahan777@hotmail.com>
2022-11-18 21:30:09 -0600

    Adds view classes for user create, update, delete

--- stock_home/views.py
+++ stock_home/views.py
@@ -1,8 +1,13 @@
 """Define views (URLS)."""
+from django.urls import reverse_lazy
 from django.views import generic
 from django.urls import reverse_lazy
 from users.models import CustomUser
 
+from stock_home.forms import (  # CompanyCreateForm,; CompanyUpdateForm,; PositionCreateForm,; PositionUpdateForm,; TransactionCreateForm,; TransactionUpdateForm,
+    UserCreateForm,
+    UserUpdateForm,
+)
 from stock_home.models import Company, Position, Transaction
 from stock_home.forms import CompanyCreateForm, CompanyUpdateForm
 
@@ -20,6 +25,34 @@ class UserDetailView(generic.DetailView):
 
     model = CustomUser
     template_name = "users/custom_user_detail.html"
+
+
+class UserCreateView(generic.CreateView):
+    """User create view."""
+
+    form_class = UserCreateForm
+    template_name = "users/custom_user_create.html"
+    # which page to show upon success
+    # replace "home" with one of the names in urls_patterns of urls.py
+    success_url = reverse_lazy("stock_home:custom_user_list")
+
+
+class UserUpdateView(generic.UpdateView):
+    """User update view."""
+
+    form_class = UserUpdateForm
+    template_name = "users/custom_user_update.html"
+    # which page to show upon success
+    success_url = reverse_lazy("stock_home:custom_user_list")
+
+
+class UserDeleteView(generic.DeleteView):
+    """<SomeModel> delete view."""
+
+    model = CustomUser
+    template_name = "users/custom_user_confirm_delete.html"
+    # which page to show upon success
+    success_url = reverse_lazy("stock_home:custom_user_list")
 
 
 # -------------------------------------------------- COMPANIES
