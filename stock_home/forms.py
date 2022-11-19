aff977baa1a4df8f0df466aa03dfbf43741ff9df
theokoles <gabrieltrahan777@hotmail.com>
2022-11-18 21:29:12 -0600

    Creates stock_home/forms.py and adds user forms

--- stock_home/forms.py
+++ stock_home/forms.py
@@ -1,8 +1,34 @@
 """Forms for Stock Home app."""
 from django.forms import ModelForm
-from stock_home.models import Company
+from users.models import CustomUser
+
+from stock_home.models import Company  # , Position, Transaction
 
 
+# -------------------------------------------------- USERS
+class UserCreateForm(ModelForm):
+    """User creation form."""
+
+    class Meta:
+        """Meta class."""
+
+        model = CustomUser
+        # list of fields to be used in the form.
+        fields = ("name", "email", "password", "account_type")
+
+
+class UserUpdateForm(ModelForm):
+    """User update form."""
+
+    class Meta:
+        """Meta class."""
+
+        model = CustomUser
+        # list of fields to be used in the form.
+        fields = ("name", "email", "password", "account_type")
+
+
+# -------------------------------------------------- COMPANIES
 class CompanyCreateForm(ModelForm):
     """Company creation form."""
 
@@ -10,11 +36,7 @@ class CompanyCreateForm(ModelForm):
         """Meta class."""
 
         model = Company
-        fields = (
-            "name", "symbol", "value", "share_price", "curr_day_open", "prev_day_open",
-            "curr_day_high", "curr_day_low", "year_high", "year_low", "div_yield",
-            "volume", "avg_volume"
-        )
+        fields = ("name", "symbol", "value", "share_price", "div_yield")
 
 
 class CompanyUpdateForm(ModelForm):
@@ -24,8 +46,12 @@ class CompanyUpdateForm(ModelForm):
         """Meta class."""
 
         model = Company
-        fields = (
-            "name", "symbol", "value", "share_price", "curr_day_open", "prev_day_open",
-            "curr_day_high", "curr_day_low", "year_high", "year_low", "div_yield",
-            "volume", "avg_volume"
-        )
+        fields = ("name", "symbol", "value", "share_price", "div_yield")
+
+
+# -------------------------------------------------- POSITIONS
+# For fields, include: user, company, quantity
+
+
+# -------------------------------------------------- TRANSACTIONS
+# For fields, include: user, company, quantity, type, price, date
