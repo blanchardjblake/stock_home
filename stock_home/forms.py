5ed5d832f29ebe76043990b7d448ca0f084f4734
theokoles <gabrieltrahan777@hotmail.com>
2022-11-18 21:29:12 -0600

    Creates stock_home/forms.py and adds user forms

--- stock_home/forms.py
+++ stock_home/forms.py
@@ -1,5 +1,7 @@
 """Forms for Stock Home app."""
 from django.forms import ModelForm
+from users.models import CustomUser
+
 from stock_home.models import Company
 
 
@@ -11,9 +13,19 @@ class CompanyCreateForm(ModelForm):
 
         model = Company
         fields = (
-            "name", "symbol", "value", "share_price", "curr_day_open", "prev_day_open",
-            "curr_day_high", "curr_day_low", "year_high", "year_low", "div_yield",
-            "volume", "avg_volume"
+            "name",
+            "symbol",
+            "value",
+            "share_price",
+            "curr_day_open",
+            "prev_day_open",
+            "curr_day_high",
+            "curr_day_low",
+            "year_high",
+            "year_low",
+            "div_yield",
+            "volume",
+            "avg_volume",
         )
 
 
@@ -25,7 +37,52 @@ class CompanyUpdateForm(ModelForm):
 
         model = Company
         fields = (
-            "name", "symbol", "value", "share_price", "curr_day_open", "prev_day_open",
-            "curr_day_high", "curr_day_low", "year_high", "year_low", "div_yield",
-            "volume", "avg_volume"
+            "name",
+            "symbol",
+            "value",
+            "share_price",
+            "curr_day_open",
+            "prev_day_open",
+            "curr_day_high",
+            "curr_day_low",
+            "year_high",
+            "year_low",
+            "div_yield",
+            "volume",
+            "avg_volume",
         )
+
+
+# -------------------------------------------------- USERS
+class UserCreateForm(ModelForm):
+    """User creation form."""
+
+    class Meta:
+        """Meta class."""
+
+        model = CustomUser
+        # list of fields to be used in the form.
+        fields = ("name", "email", "password", "account_type")
+
+
+class UserUpdateForm(ModelForm):
+    """User update form."""
+
+    class Meta:
+        """Meta class."""
+
+        model = CustomUser
+        # list of fields to be used in the form.
+        fields = ("name", "email", "password", "account_type")
+
+
+# -------------------------------------------------- COMPANIES
+# For fields, include: name, symbol, value, share_price, div_yeild
+
+
+# -------------------------------------------------- POSITIONS
+# For fields, include: user, company, quantity
+
+
+# -------------------------------------------------- TRANSACTIONS
+# For fields, include: user, company, quantity, type, price, date
