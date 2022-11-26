65110170165e8736ba9456b0281ac441ee4edf66
theokoles <gabrieltrahan777@hotmail.com>
2022-11-25 20:56:09 -0600

    Adds path for Stock Home landing page

--- stock_home/urls.py
+++ stock_home/urls.py
@@ -7,6 +7,7 @@ from stock_home import views
 app_name = "stock_home"
 
 urlpatterns = [
+    path("", views.LandingPageView.as_view(), name="landing_page"),
     path("company", views.CompanyListView.as_view(), name="company_list"),
     path("company/read/<int:pk>/", views.CompanyDetailView.as_view(), name="company_detail"),
     path("company/create/", views.CompanyCreateView.as_view(), name="company_create"),
