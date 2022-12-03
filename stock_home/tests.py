224ab3f27e33d68f02aee6850029a1572095b1d6
theokoles <gabrieltrahan777@hotmail.com>
2022-12-03 08:41:34 -0600

    Adds test for creating Company with no name

--- stock_home/tests.py
+++ stock_home/tests.py
@@ -64,3 +64,24 @@ class CompanyTestCase(TestCase):
                 avg_volume=30000000,
             )
         self.assertEqual(exc_info.type, IntegrityError)
+
+    def test_create_company_raises_missing_data_integrity_error_with_no_name(self):
+        """Tests if `Company`'s `create()` method raises an integrity error."""
+
+        with pytest.raises(IntegrityError) as exc_info:
+            Company.objects.create(
+                name=None,
+                symbol="FCOM",
+                value=10000000,
+                share_price=10.00,
+                curr_day_open=9.90,
+                prev_day_open=9.95,
+                curr_day_high=10.10,
+                curr_day_low=9.86,
+                year_high=14.00,
+                year_low=6.72,
+                div_yield=0.30,
+                volume=34000000,
+                avg_volume=30000000,
+            )
+        self.assertEqual(exc_info.type, IntegrityError)
